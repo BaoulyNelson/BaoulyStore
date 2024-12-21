4820bcd23aa1ca480e0bfad80157e7dc571e0b32
BaoulyNelson <elconquistadorbaoulyn@gmail.com>
2024-12-20 22:49:01 -0500

    Ajout de baoulystore.onrender.com à ALLOWED_HOSTS

--- BaoulyStore/settings.py
+++ BaoulyStore/settings.py
@@ -18,6 +18,9 @@ DEBUG = config('DJANGO_DEBUG', cast=bool)
 # ALLOWED_HOSTS (charge depuis le fichier .env et sépare les valeurs par des virgules)
 ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
 
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
